27efe4b3c0fd915ca61b672286b97ec9ff26db44
Nunnyr <nunnyreyes@gmail.com>
2020-08-04 15:23:56 -0400

    trying to add happiness

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,28 @@
 # your code goes here
+require 'pry'
+class Person
+
+    attr_reader :name 
+    # attr_writer :happiness, :hygiene
+     attr_accessor :bank_account, :happiness, :hygiene
+
+    def initialize (name_arg)
+        @name = name_arg
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+
+    end
+
+    def happiness=(num)
+        @happiness = num
+
+    
+
+end
+
+who = Person.new("Blake")
+
+# binding.pry
+# 0
+
