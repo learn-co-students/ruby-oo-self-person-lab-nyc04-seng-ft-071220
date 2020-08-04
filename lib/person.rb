76080830792f0963c3dc3aa14a7d7f02e8601907
attenbl19 <attenboroughl@protonmail.com>
2020-08-04 16:09:34 -0400

    OO Person Solo Lab framework

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,56 @@
 # your code goes here
+class Person
+    #attr_reader :name
+
+    attr_accessor :bank_account, :happiness, :hygiene
+
+    def initialize(name, bank_account, happiness, hygiene)
+       @name = name
+       @bank_account = 25
+       @happiness = 8
+       @hygiene = 8
+
+    end
+
+
+
+    def happy?
+        if happy? > 7
+             true
+        else false
+    end
+
+    def clean?
+        if clean? > 7
+           true
+            else false
+    end
+
+    def get_paid(salary)
+        get_paid += bank_account
+        "all  about the benjamins "
+        end
+
+    def take_bath
+        @hygiene += 4
+            returns  "song"
+    end
+
+    def work_out
+        work_out = 3
+    end
+
+    def call_friend(name)
+        @happiness += 3
+
+
+
+
+
+
+
+
+
+
+end
+
