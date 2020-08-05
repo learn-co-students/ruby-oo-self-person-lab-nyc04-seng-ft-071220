941d0f2fb7431bb512bcb321a81339c767153adc
Nunnyr <nunnyreyes@gmail.com>
2020-08-05 13:09:45 -0400

    added working out

--- lib/person.rb
+++ lib/person.rb
@@ -2,24 +2,35 @@
 require 'pry'
 class Person
 
-    attr_reader :name
+    attr_reader :name 
     attr_accessor :bank_account, :happiness, :hygiene
 
     def initialize(name_arg)
         @name = name_arg
         @bank_account = 25
-        @happiness = 8
+        @happiness = 8 
         @hygiene = 8
 
     end
 
     def happiness
+
         @happiness = 0 if @happiness < 0
         @happiness = 10 if @happiness > 10
-        @happiness
+        @happiness #implicit return
+
 
     end
-    
+
+# #####to read
+#     def name
+#         @name
+#     end
+# ####to write
+#     def name=(name)
+#         @name = name
+#     end
+
     def hygiene
     @hygiene = 0 if @hygiene < 0
     @hygiene = 10 if @hygiene > 10
@@ -27,9 +38,36 @@ class Person
     end
 
 
+    def happy?
+        @happiness > 7
+    end
+
+    def clean?
+        @hygiene > 7
+    end
+
+    def get_paid(salary)
+        @bank_account = @bank_account + salary
+        p 'all about the benjamins'
+    end
+
+    def take_bath
+        self.hygiene += 4
+        '♪ Rub-a-dub just relaxing in the tub ♫'
+    end
+
+    def work_out
+        self.hygiene -= 3
+        self.happiness += 2
+        '♪ another one bites the dust ♫'
+
+
+
+    end
+
 end
 
-who = Person.new("Blake")
+
 
 # binding.pry
 # 0
