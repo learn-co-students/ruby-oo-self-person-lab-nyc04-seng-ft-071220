21c212dabaf4c5cc6ce21e81c626fc1eee28ed93
nisayana <nisayana23@yandex.ru>
2020-08-06 16:11:14 -0400

    First commit

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,21 @@
 # your code goes here
+require 'pry'
+
+class Person
+
+    attr_reader :bank_account
+
+    attr_accessor :name, :happiness, :hygiene
+
+    def initialize(name)
+        @name = name
+    end
+
+    # def clean?
+        
+    # end
+
+    # def name 
+    #     @name
+    # end
+end
